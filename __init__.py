84a9a250ad6e23307a11fd0b2a39b0fe28f02052
Rob <rob_dodd6@hotmail.com>
2020-02-28 20:16:24 +0000

    added roomba module

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+from Roomba980.roomba import password
 
 class Test(MycroftSkill):
     def __init__(self):
@@ -7,9 +7,31 @@ class Test(MycroftSkill):
 
     @intent_file_handler('test.intent')
     def handle_test(self, message):
-        self.speak_dialog('test')
+        self.speak_dialog('test2')
+        self.log.info("Test handler called")
+        newpass = password()
 
+    @intent_file_handler('TestColour.intent')
+    def handle_test_colour(self, message):
+        colour = message.data.get('colour')
+        self.log.info("User asked about colour " + colour)
+
+    def initialize(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def converse(self, utterances, lang):
+        self.log.info("Converse called with utterance " + utterances[0])
+        return True
+        # if utterances and self.voc_match(utterances[0], 'understood'):
+        #     self.speak_dialog('great')
+        #     return True
+        # else:
+        #     return False
 
 def create_skill():
+
     return Test()
 
